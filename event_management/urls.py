91f53db32524bc94b6bb9e221781c31c5408d9d5
unknown <jaya.maheswari.lakshmi@gmail.com>
2026-08-07 16:20:38 +0530

    Allow Django to serve media files in production

--- event_management/urls.py
+++ event_management/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls')),
+    path('', include('website.urls')), # Replace 'website' with your app name if different
+    
+    # Enables serving media files on Render when DEBUG = False
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
